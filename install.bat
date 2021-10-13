46fab37463d3c585f105e11c3392b775293adff8
seanbf <83425701+seanbf@users.noreply.github.com>
2021-10-13 13:38:57 +0100

    Add files via upload

--- install.bat
+++ install.bat
@@ -28,10 +28,36 @@ goto start
 :install_full
 ECHO Installing Python 3.7.4, Paths, Dependancies and Launching Motor Loss Tool
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PowerShellScriptPath%""' -Verb RunAs}"
+
+ECHO Generating Desktop Shortcut and Icon
+set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
+echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
+echo sLinkFile = "%USERPROFILE%\Desktop\Motor Loss Tool.lnk" >> %SCRIPT%
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
+echo oLink.TargetPath ="%comspec%" >> %SCRIPT%
+echo oLink.Arguments = " /c ""%ThisScriptsDirectory%motor_loss_tool.bat""" >> %SCRIPT%
+echo oLink.IconLocation = "%ThisScriptsDirectory%\icon\motor.ico" >> %SCRIPT%
+echo oLink.Save >> %SCRIPT%
+cscript /nologo %SCRIPT%
+del %SCRIPT%
+
 goto end
 
 :install_dep
 ECHO Installing Dependancies and Launching Motor Loss Tool
+
+ECHO Generating Desktop Shortcut and Icon
+set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
+echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
+echo sLinkFile = "%USERPROFILE%\Desktop\Motor Loss Tool.lnk" >> %SCRIPT%
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
+echo oLink.TargetPath ="%comspec%" >> %SCRIPT%
+echo oLink.Arguments = " /c ""%ThisScriptsDirectory%motor_loss_tool.bat""" >> %SCRIPT%
+echo oLink.IconLocation = "%ThisScriptsDirectory%\icon\motor.ico" >> %SCRIPT%
+echo oLink.Save >> %SCRIPT%
+cscript /nologo %SCRIPT%
+del %SCRIPT%
+
 py -3.7 -m pip install -r %ThisScriptsDirectory%\program\install\requirements.txt
 py -3.7 -m streamlit run %ThisScriptsDirectory%\program\motor_loss_tool.py
 goto end
